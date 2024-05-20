97e2dacad4e9861bc31773e563426a3b2ad9bc39
Roman <highrat@gmail.com>
2024-05-20 16:39:33 +0300

    Homework

--- script/script.js
+++ script/script.js
@@ -1,52 +1,52 @@
 const globalCardsData = {
-    cards: [
-        {
-            id: 1,
-            image: "./img/news_image01.jpg",
-            design: "Kitchan Design",
-            heading: "Let’s Get Solution For Building Construction Work",
-            timing: "26 December,2022",
-        },
-        {
-            id: 2,
-            image: "img/news_image02.jpg",
-            design: "Living Design",
-            heading: "Low Cost Latest Invented Interior Designing Ideas.",
-            timing: "22 December,2022",
-        },
-        {
-            id: 3,
-            image: "img/news_image03.jpg",
-            design: "Interior Design",
-            heading: "Best For Any Office & Business Interior Solution",
-            timing: "25 December,2022",
-        },
-        {
-            id: 4,
-            image: "img/news_image04.jpg",
-            design: "Kitchan Design",
-            heading: "Let’s Get Solution For Building Construction Work",
-            timing: "26 December,2022",
-        },
-        {
-            id: 5,
-            image: "img/news_image05.jpg",
-            design: "Living Design",
-            heading: "Low Cost Latest Invented Interior Designing Ideas.",
-            timing: "22 December,2022",
-        },
-        {
-            id: 6,
-            image: "img/news_image06.jpg",
-            design: "Interior Design",
-            heading: "Best For Any Office & Business Interior Solution",
-            timing: "25 December,2022",
-        },
-    ],
+  cards: [
+    {
+      id: 1,
+      image: "./img/news_image01.jpg",
+      design: "Kitchan Design",
+      heading: "Let’s Get Solution For Building Construction Work",
+      timing: "26 December,2022",
+    },
+    {
+      id: 2,
+      image: "img/news_image02.jpg",
+      design: "Living Design",
+      heading: "Low Cost Latest Invented Interior Designing Ideas.",
+      timing: "22 December,2022",
+    },
+    {
+      id: 3,
+      image: "img/news_image03.jpg",
+      design: "Interior Design",
+      heading: "Best For Any Office & Business Interior Solution",
+      timing: "25 December,2022",
+    },
+    {
+      id: 4,
+      image: "img/news_image04.jpg",
+      design: "Kitchan Design",
+      heading: "Let’s Get Solution For Building Construction Work",
+      timing: "26 December,2022",
+    },
+    {
+      id: 5,
+      image: "img/news_image05.jpg",
+      design: "Living Design",
+      heading: "Low Cost Latest Invented Interior Designing Ideas.",
+      timing: "22 December,2022",
+    },
+    {
+      id: 6,
+      image: "img/news_image06.jpg",
+      design: "Interior Design",
+      heading: "Best For Any Office & Business Interior Solution",
+      timing: "25 December,2022",
+    },
+  ],
 };
 
 Vue.component("page-header", {
-    template: `
+  template: `
 <header class="header center">
   <div class="header__logo">
     <img src="img/Logo.svg" alt="logo" />
@@ -61,7 +61,7 @@ Vue.component("page-header", {
 });
 
 Vue.component("main__banner", {
-    template: `
+  template: `
 <section class="baner center">
   <div class="baner__wrap">
     <div class="baner__wrap-content">
@@ -82,8 +82,8 @@ Vue.component("main__banner", {
 });
 
 Vue.component("banner", {
-    props: ["image", "box"],
-    template: `
+  props: ["image", "box"],
+  template: `
 <section class="banner center" :style="{backgroundImage:'url(' + image + ')', backgroundRepeat:'no-repeat', backgroundPosition:'center center', backgroundSize: 'cover'}">
       <div v-if="box" class="banner__intro">
         <h1 class="banner__intro-title">Articles & News {{box}}</h1>
@@ -93,7 +93,7 @@ Vue.component("banner", {
 });
 
 var main__project = {
-    template: `
+  template: `
   <section class="project center">
     <h2 class="project__title">Follow Our Projects</h2>
     <p class="project__text">
@@ -166,7 +166,7 @@ var main__project = {
     `,
 };
 var main__counter = {
-    template: `
+  template: `
   <section class="counter center">
     <div class="counter__wrap">
       <div class="counter__item">
@@ -191,7 +191,7 @@ var main__counter = {
 };
 
 Vue.component("page-footer", {
-    template: `    
+  template: `    
 <footer class="footer center">
   <div class="footer__logo">
     <div class="header__logo">
@@ -248,8 +248,8 @@ Vue.component("page-footer", {
 });
 
 Vue.component("product", {
-    props: ["image", "design", "heading", "timing"],
-    template: `
+  props: ["image", "design", "heading", "timing"],
+  template: `
   <div class="blog__item">
     <div class="blog__item-image">
       <img :src="image" :alt="image" />
@@ -269,11 +269,11 @@ Vue.component("product", {
 });
 
 Vue.component("products_small", {
-    data() {
-        return globalCardsData;
-    },
-    props: ["counter"],
-    template: `
+  data() {
+    return globalCardsData;
+  },
+  props: ["counter"],
+  template: `
   <div class="blog__items">
       <product v-for="(card, index) in cards.slice(0, 3)" :key="card.id" :image="card.image" :design="card.design" :heading="card.heading" :timing="card.timing" :class="{ 'blog__item-select': index === 1 }"></product>
   </div>
@@ -281,11 +281,11 @@ Vue.component("products_small", {
 });
 
 Vue.component("products_all", {
-    data() {
-        return globalCardsData;
-    },
-    props: ["counter"],
-    template: `
+  data() {
+    return globalCardsData;
+  },
+  props: ["counter"],
+  template: `
   <div class="blog__items" >
     <product v-for="(card, index) in cards" :key="card.id" :image="card.image" :design="card.design" :heading="card.heading" :timing="card.timing" :class="{ 'blog__item-select': index === 1 }"></product>
   </div>
@@ -293,11 +293,11 @@ Vue.component("products_all", {
 });
 
 Vue.component("main_article_and_news", {
-    data() {
-        return globalCardsData;
-    },
-    props: ["counter"],
-    template: `
+  data() {
+    return globalCardsData;
+  },
+  props: ["counter"],
+  template: `
 <section class="blog center">
   <h2 class="blog__title">Articles & News</h2>
   <p class="blog__text">
@@ -312,11 +312,11 @@ Vue.component("main_article_and_news", {
 });
 
 Vue.component("blog_article_and_news", {
-    data() {
-        return globalCardsData;
-    },
-    props: ["counter"],
-    template: `
+  data() {
+    return globalCardsData;
+  },
+  props: ["counter"],
+  template: `
 <section class="blog center">
   <h2 class="blog__title">Articles & News</h2>
   <p class="blog__text">
@@ -330,28 +330,29 @@ Vue.component("blog_article_and_news", {
 });
 
 Vue.component("tagsbox", {
-    props: ["tagsarray", "articlesarray"],
-    methods: {
-        selecttag(index) {
-            // `this` в методе указывает на текущий активный экземпляр
-            console.log("Привет, " + tagsarray[index] + "!");
-            // articlesarray = articles.forEach.filter(
-            //     (item) => item.tag === tagsarray[index]
-            // );
-            console.log(articlesarray);
-            console.log(
-                articlesarray.filter((item) => item.tag === tagsarray[index])
-            );
-            return articlesarray.filter(
-                (item) => item.tag === tagsarray[index]
-            );
-        },
+  data() {
+    return tagsarray, articlesarray;
+  },
+  props: ["tagsarray", "articlesarray"],
+  methods: {
+    selecttag(index) {
+      // `this` в методе указывает на текущий активный экземпляр
+      console.log("Привет, " + tagsarray[index] + "!");
+      // articlesarray = articles.forEach.filter(
+      //     (item) => item.tag === tagsarray[index]
+      // );
+      console.log(articlesarray);
+      console.log(
+        articlesarray.filter((item) => item.tag === tagsarray[index])
+      );
+      return articlesarray.filter((item) => item.tag === tagsarray[index]);
     },
-    getArticles: function () {
-        return articlesarray.filter((item) => item.tag === tagsarray[index]);
-    },
+  },
+  getArticles: function () {
+    return articlesarray.filter((item) => item.tag === tagsarray[index]);
+  },
 
-    template: `
+  template: `
 <div class="tags__container">
     <div @click="selecttag(index)" class="tags__item" v-for="(tag, index) in tagsarray" :key="index">{{tag}}</div>
 </div>
@@ -359,8 +360,8 @@ Vue.component("tagsbox", {
 });
 
 Vue.component("blogarticle", {
-    props: ["articlesarray", "getArticles"],
-    template: `
+  props: ["articlesarray", "getArticles"],
+  template: `
 <div class="articles">
 	<div class="blogarticle" v-for="(blogarticle, index) in articlesarray" :key="index">   
         <h2 class="blogarticle__header">
@@ -391,32 +392,31 @@ Vue.component("blogarticle", {
 });
 
 Vue.component("tagsandarticles", {
-    props: ["tagsarray", "articlesarray"],
-    methods: {
-        selecttag() {
-            // `this` в методе указывает на текущий активный экземпляр
-            console.log("Привет, " + tagsarray[index] + "!");
+  data() {
+    return tagsarray, articlesarray;
+  },
+  props: ["tagsarray", "articlesarray"],
+  methods: {
+    selecttag() {
+      // `this` в методе указывает на текущий активный экземпляр
+      console.log("Привет, " + tagsarray[index] + "!");
 
-            console.log(articlesarray);
-            console.log(
-                articlesarray.filter((item) => item.tag === tagsarray[index])
-            );
-            return articlesarray.filter(
-                (item) => item.tag === tagsarray[index]
-            );
-        },
+      console.log(articlesarray);
+      console.log(
+        articlesarray.filter((item) => item.tag === tagsarray[index])
+      );
+      return articlesarray.filter((item) => item.tag === tagsarray[index]);
     },
-    computed: {
-        getArticles: function (index) {
-            if (index === undefined) {
-                return articlesarray;
-            }
-            return articlesarray.filter(
-                (item) => item.tag === tagsarray[index]
-            );
-        },
+  },
+  computed: {
+    getArticles: function (index) {
+      if (index === undefined) {
+        return articlesarray;
+      }
+      return articlesarray.filter((item) => item.tag === tagsarray[index]);
     },
-    template: `
+  },
+  template: `
   <div class="articles">
 	  <div class="blogarticle" v-for="(blogarticle, index) in selecttag" :key="index">   
         <h2 class="blogarticle__header">
@@ -454,23 +454,23 @@ Vue.component("tagsandarticles", {
 });
 
 new Vue({
-    el: "#app1",
-    data: {
-        articlesarray: articlesarray,
-        tagsarray: tagsarray,
-    },
-    components: {
-        // main__banner: main__banner,
-        main__project: main__project,
-        main__counter: main__counter,
-    },
+  el: "#app1",
+  data: {
+    articlesarray: articlesarray,
+    tagsarray: tagsarray,
+  },
+  components: {
+    // main__banner: main__banner,
+    main__project: main__project,
+    main__counter: main__counter,
+  },
 });
 new Vue({
-    el: "#app2",
-    data: {
-        tagsarray: tagsarray,
-    },
+  el: "#app2",
+  data: {
+    tagsarray: tagsarray,
+  },
 });
 new Vue({
-    el: "#app3",
+  el: "#app3",
 });
